427a74e2bcbf1bf16e1fd82ec8705818e0d1cac6
曾思信 <zengsixin@manlinggame.com>
2023-11-09 15:03:43 +0800

    测试

---
+++ WinForms_FGUI/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace WinForms_FGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fguiPath = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtConsole = new System.Windows.Forms.TextBox();
+            this.btnPackage = new System.Windows.Forms.Button();
+            this.btnRef = new System.Windows.Forms.Button();
+            this.fguiPKGTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ignoreTxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ignoreIconCommon = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // fguiPath
+            // 
+            this.fguiPath.Location = new System.Drawing.Point(66, 6);
+            this.fguiPath.Name = "fguiPath";
+            this.fguiPath.Size = new System.Drawing.Size(376, 23);
+            this.fguiPath.TabIndex = 0;
+            this.fguiPath.Text = "G:\\Bingganren2021_SVN\\client\\FguiProject\\assets";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(552, 75);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(104, 25);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "图片无引用的";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtConsole
+            // 
+            this.txtConsole.Location = new System.Drawing.Point(12, 114);
+            this.txtConsole.Multiline = true;
+            this.txtConsole.Name = "txtConsole";
+            this.txtConsole.Size = new System.Drawing.Size(776, 735);
+            this.txtConsole.TabIndex = 2;
+            // 
+            // btnPackage
+            // 
+            this.btnPackage.Location = new System.Drawing.Point(221, 38);
+            this.btnPackage.Name = "btnPackage";
+            this.btnPackage.Size = new System.Drawing.Size(77, 24);
+            this.btnPackage.TabIndex = 3;
+            this.btnPackage.Text = "拥有的包";
+            this.btnPackage.UseVisualStyleBackColor = true;
+            this.btnPackage.Click += new System.EventHandler(this.btnPackage_Click);
+            // 
+            // btnRef
+            // 
+            this.btnRef.Location = new System.Drawing.Point(304, 38);
+            this.btnRef.Name = "btnRef";
+            this.btnRef.Size = new System.Drawing.Size(102, 24);
+            this.btnRef.TabIndex = 4;
+            this.btnRef.Text = "查询依赖";
+            this.btnRef.UseVisualStyleBackColor = true;
+            this.btnRef.Click += new System.EventHandler(this.btnRef_Click);
+            // 
+            // fguiPKGTxt
+            // 
+            this.fguiPKGTxt.Location = new System.Drawing.Point(446, 76);
+            this.fguiPKGTxt.Name = "fguiPKGTxt";
+            this.fguiPKGTxt.Size = new System.Drawing.Size(100, 23);
+            this.fguiPKGTxt.TabIndex = 5;
+            this.fguiPKGTxt.Text = "Common";
+            this.fguiPKGTxt.TextChanged += new System.EventHandler(this.fguiPKGTxt_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(267, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "若不填(空白),则默认为整个工程";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "工程目录";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "公共包";
+            // 
+            // ignoreTxt
+            // 
+            this.ignoreTxt.Location = new System.Drawing.Point(55, 38);
+            this.ignoreTxt.Name = "ignoreTxt";
+            this.ignoreTxt.Size = new System.Drawing.Size(160, 23);
+            this.ignoreTxt.TabIndex = 9;
+            this.ignoreTxt.Text = "Common;Items;Font";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "icon包;常驻包";
+            // 
+            // ignoreIconCommon
+            // 
+            this.ignoreIconCommon.Location = new System.Drawing.Point(91, 76);
+            this.ignoreIconCommon.Name = "ignoreIconCommon";
+            this.ignoreIconCommon.Size = new System.Drawing.Size(160, 23);
+            this.ignoreIconCommon.TabIndex = 11;
+            this.ignoreIconCommon.Text = "Items;Common";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 861);
+            this.Controls.Add(this.ignoreIconCommon);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ignoreTxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fguiPKGTxt);
+            this.Controls.Add(this.btnRef);
+            this.Controls.Add(this.btnPackage);
+            this.Controls.Add(this.txtConsole);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.fguiPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox fguiPath;
+        private Button btnSearch;
+        private TextBox txtConsole;
+        private Button btnPackage;
+        private Button btnRef;
+        private TextBox fguiPKGTxt;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox ignoreTxt;
+        private Label label4;
+        private TextBox ignoreIconCommon;
+    }
+}
---
+++ WinForms_FGUI/Form1.cs
@@ -0,0 +1,402 @@
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Linq;
+
+namespace WinForms_FGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string mFGUIPath = @"C:/fguiPath.txt";
+        Dictionary<string, string> mPackageUIdNameDic = new Dictionary<string, string>();//key=包id,,,value=包名字
+        Dictionary<string, List<string>> mXmlListDic = new Dictionary<string, List<string>>();
+        Dictionary<string, string> mCommonNameUIdDic;// = new Dictionary<string, string>() { { "Common", "" }, { "Items", "" }, { "Font", "" }, };//公有包  key=包名字,,,value=包id
+        Dictionary<string, string> mNoneCommonUIdNameDic;//= new Dictionary<string, string>() { { "Common", "" }, { "Items", "" }, { "Font", "" }, };//公有包  key=包名字,,,value=包id
+        private void btnPackage_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.ignoreTxt.Text))
+            {
+                return;
+            }
+            var strs = this.ignoreTxt.Text.Split(";");
+            mCommonNameUIdDic = new Dictionary<string, string>();
+            for (int i = 0; i < strs.Length; i++)
+            {
+                mCommonNameUIdDic.Add(strs[i], "");
+            }
+
+            var contentTxt = this.fguiPath.Text + "_*_" + this.ignoreTxt.Text + "_*_" + this.fguiPKGTxt.Text+"_*_"+this.ignoreIconCommon.Text;
+            File.WriteAllText(mFGUIPath, contentTxt);
+
+            mPackageUIdNameDic.Clear();
+            mNoneCommonUIdNameDic = new Dictionary<string, string>();
+            string directoryPath = this.fguiPath.Text;// 目录路径
+            if (Directory.Exists(directoryPath) == false)
+            {
+                MessageBox.Show("目录不存在");
+                return;
+            }
+            string[] xmlFiles = Directory.GetFiles(directoryPath, "*.xml", SearchOption.AllDirectories); // 获取以.xml为后缀的所有文件
+            string packagePattern = @"id=""([^""]+)""";
+
+            string[] strTxt;
+
+            foreach (string file in xmlFiles)
+            {
+                if (file.Contains("package.xml"))
+                {
+                    strTxt = File.ReadAllLines(file);
+
+                    if (strTxt[1].Contains("packageDescription"))
+                    {
+                        string packageUId = Regex.Match(strTxt[1], packagePattern).Groups[1].Value;
+                        var tSplit = file.Split("\\", StringSplitOptions.None);
+                        var packageName = tSplit[tSplit.Length - 2];
+                        mPackageUIdNameDic[packageUId] = packageName;
+                        if (mCommonNameUIdDic.ContainsKey(packageName))
+                        {
+                            mCommonNameUIdDic[packageName] = packageUId;
+                        }
+                        else
+                        {
+                            mNoneCommonUIdNameDic[packageUId] = packageName;
+                        }
+                    }
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in mPackageUIdNameDic)
+            {
+                sb.AppendLine(item.Key + "__" + item.Value);// +";");
+            }
+            this.txtConsole.Text = sb.ToString();
+        }
+
+
+        private void btnRef_Click(object sender, EventArgs e)
+        {
+            if (mCommonNameUIdDic == null)
+            {
+                MessageBox.Show("请先点击 拥有的包 按钮");
+                return;
+            }
+            mXmlListDic.Clear();
+            string directoryPath = this.fguiPath.Text;// 目录路径
+            string[] xmlFiles = Directory.GetFiles(directoryPath, "*.xml", SearchOption.AllDirectories); // 获取以.xml为后缀的所有文件
+            string pkgPattern = @"pkg=""([^""]+)""";
+            string urlPattern = @"url=""([^""]+)""";//url="ui://m9pqa398gbxfe9l" 
+            string[] strTxt;
+            foreach (string file in xmlFiles)
+            {
+                if (file.Contains("package.xml") == false)
+                {
+                    strTxt = File.ReadAllLines(file);
+
+                    var tSplit = file.Split("\\", StringSplitOptions.None);
+                    var xmlName = tSplit[tSplit.Length - 1]; //***.xml                 
+                    var tIndex = 0;
+                    for (int i = 0; i < tSplit.Length; i++)
+                    {
+                        if (tSplit[i] == "assets")
+                        {
+                            tIndex = i;
+                            break;
+                        }
+                    }
+                    var selfPack = tSplit[tIndex + 1];
+
+                    for (int i = 0; i < strTxt.Length; i++)
+                    {
+                        string packageUId = Regex.Match(strTxt[i], pkgPattern).Groups[1].Value;//某包id
+                        string urlUIAll = Regex.Match(strTxt[i], urlPattern).Groups[1].Value;//url全路径
+
+                        if (string.IsNullOrEmpty(packageUId) == false)
+                        {
+                            var packageName = "";
+                            mPackageUIdNameDic.TryGetValue(packageUId, out packageName);
+                            //var packageName = mPackageUIdNameDic.ContainsKey(packageUId)? mPackageUIdNameDic[packageUId]:"";//某包名字
+                            if (string.IsNullOrEmpty(packageName) == false && mCommonNameUIdDic.ContainsKey(packageName) == false && packageName != selfPack)
+                            {
+                                AddDicTry(selfPack, xmlName, packageName);
+                            }
+                        }
+                        else if (string.IsNullOrEmpty(urlUIAll) == false)
+                        {
+                            foreach (var item in mNoneCommonUIdNameDic)////key=包id,,,value=包名字
+                            {
+                                if (urlUIAll.Contains(item.Key) && item.Value != selfPack)
+                                {
+                                    AddDicTry(selfPack, xmlName, item.Value);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("1.可能丢失的引用 也计算在内了(若FGUI中查询我的依赖的资源 并没找到时,应该是丢失了,理论上丢失的也要把其清空的)  2.只能找到直接引用,间接引用无法查询");
+            sb.AppendLine();
+            foreach (var item in mXmlListDic)
+            {
+                var str = "";
+                for (int i = 0; i < item.Value.Count; i++)
+                {
+                    str += item.Value[i] + ";";
+                }
+                sb.AppendLine(item.Key + " 依赖了其他业务包 : " + str);
+            }
+            this.txtConsole.Text = sb.ToString();
+
+        }
+
+        void AddDicTry(string selfPack, string xmlName, string Value)
+        {
+            var name = selfPack + "包的" + xmlName;
+            if (mXmlListDic.ContainsKey(name))
+            {
+                if (mXmlListDic[name].Contains(Value) == false)
+                {
+                    mXmlListDic[name].Add(Value);
+                }
+            }
+            else
+            {
+                mXmlListDic[name] = new List<string>();
+                mXmlListDic[name].Add(Value);
+            }
+        }
+
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (this.ignoreIconCommon.Text.Length <= 2 || this.ignoreIconCommon.Text.Contains(";")==false)
+            {
+                return;
+            }
+
+            var ignoreList = this.ignoreIconCommon.Text.Split(";");
+
+            string directoryPath;
+            if (this.fguiPKGTxt.Text.Length <= 2)
+            {
+                directoryPath = this.fguiPath.Text;//全路径
+            }
+            else
+            {
+                directoryPath = this.fguiPath.Text + "/" + this.fguiPKGTxt.Text;// 目录路径
+            }
+            var isPath = (directoryPath.Contains("Fgui") || directoryPath.Contains("fgui"));
+            if (isPath == false)
+            {
+                MessageBox.Show("你的目录咋不含Fgui | fgui 字眼___判定为非法目录");
+                return;
+            }
+            if (Directory.Exists(directoryPath) == false)
+            {
+                MessageBox.Show("目录不存在");
+                return;
+            }
+            var contentTxt = this.fguiPath.Text + "_*_" + this.ignoreTxt.Text + "_*_" + this.fguiPKGTxt.Text + "_*_" + this.ignoreIconCommon.Text;
+            File.WriteAllText(mFGUIPath, contentTxt);
+
+            string[] xmlFiles = Directory.GetFiles(directoryPath, "*.xml", SearchOption.AllDirectories); // 获取以.xml为后缀的所有文件
+            string packagePattern = @"id=""([^""]+)""";
+
+            string[] strTxt;
+
+            string packageId = string.Empty; ;
+            Dictionary<string, string> idNameDic = new Dictionary<string, string>();
+            Dictionary<string, string> pathPngDic = new Dictionary<string, string>();
+            Dictionary<string, string> urlIdDic = new Dictionary<string, string>();
+            string idPattern = @"id=""([^""]+)""";
+            string namePattern = @"name=""([^""]+)""";
+            string pathPattern = @"path=""([^""]+)""";
+            foreach (string file in xmlFiles)
+            {
+                if (file.Contains(ignoreList[0]) == false && file.Contains("package.xml"))
+                {
+                    strTxt = File.ReadAllLines(file);
+                    packageId = string.Empty;
+                    for (int i = 0; i < strTxt.Length; i++)
+                    {
+                        if (strTxt[i].Contains("image"))
+                        {
+                            string idValue = Regex.Match(strTxt[i], idPattern).Groups[1].Value;
+                            string nameValue = Regex.Match(strTxt[i], namePattern).Groups[1].Value;
+                            string pathValue = Regex.Match(strTxt[i], pathPattern).Groups[1].Value;
+                            idNameDic[idValue] = nameValue;
+                            pathPngDic[idValue] = file.Replace("package.xml", "") + (pathValue + nameValue);
+
+                            if (strTxt[1].Contains("packageDescription"))
+                            {
+                                string packageValue = Regex.Match(strTxt[1], packagePattern).Groups[1].Value;
+                                urlIdDic["ui://" + packageValue + idValue] = idValue;
+                            }
+                        }
+                    }
+                }
+            }
+
+            string srcPattern = @"src=""([^""]+)""";
+            string urlPattern = @"url=""([^""]+)""";
+            string iconPattern = @"icon=""([^""]+)""";
+            string iconSelectPattern = @"selectedIcon=""([^""]+)""";
+            string valusePattern = @"values=""([^""]+)""";
+            string defaultPattern = @"default=""([^""]+)""";
+            string iconItemPattern = @"icon=""([^""]+)""";//      <item icon="ui://qllwua2i9mq2w57"/>
+            string currTxt_I = "";
+            if (this.fguiPKGTxt.Text == ignoreList[1])
+            {
+                xmlFiles = Directory.GetFiles(this.fguiPath.Text, "*.xml", SearchOption.AllDirectories); // 获取以.xml为后缀的所有文件
+            }
+            foreach (string file in xmlFiles)
+            {
+                if (file.Contains("package.xml") == false)
+                {
+                    //if (file.Contains("RankView"))
+                    //{
+                    strTxt = File.ReadAllLines(file);
+                    for (int i = 0; i < strTxt.Length; i++)
+                    {
+                        currTxt_I = strTxt[i];
+                        {
+                            if (currTxt_I.Contains("src"))
+                            {
+                                string srcValue = Regex.Match(currTxt_I, srcPattern).Groups[1].Value;
+                                if (idNameDic.ContainsKey(srcValue) == true)
+                                {
+                                    idNameDic.Remove(srcValue);
+                                }
+                            }
+                            else if (currTxt_I.Contains("url"))
+                            {
+                                string srcValue = Regex.Match(currTxt_I, urlPattern).Groups[1].Value;
+                                if (urlIdDic.ContainsKey(srcValue) == true)
+                                {
+                                    idNameDic.Remove(urlIdDic[srcValue]);
+                                }
+                            }
+                            else if (currTxt_I.Contains("Button"))
+                            {
+                                string srcValue = Regex.Match(currTxt_I, iconPattern).Groups[1].Value;
+                                if (urlIdDic.ContainsKey(srcValue) == true)
+                                {
+                                    idNameDic.Remove(urlIdDic[srcValue]);
+                                }
+
+                                string srcValue2 = Regex.Match(currTxt_I, iconSelectPattern).Groups[1].Value;
+                                if (urlIdDic.ContainsKey(srcValue2) == true)
+                                {
+                                    idNameDic.Remove(urlIdDic[srcValue2]);
+                                }
+                            }
+                            else if (currTxt_I.Contains("<item "))
+                            {
+                                string srcValue = Regex.Match(currTxt_I, iconItemPattern).Groups[1].Value;
+                                if (urlIdDic.ContainsKey(srcValue) == true)
+                                {
+                                    idNameDic.Remove(urlIdDic[srcValue]);
+                                }
+
+                                string srcValue2 = Regex.Match(currTxt_I, iconSelectPattern).Groups[1].Value;
+                                if (urlIdDic.ContainsKey(srcValue2) == true)
+                                {
+                                    idNameDic.Remove(urlIdDic[srcValue2]);
+                                }
+                            }
+                            else if (currTxt_I.Contains("controller") && (currTxt_I.Contains("values=\"ui://") || currTxt_I.Contains("values=\"|")))
+                            {
+                                string srcValue = Regex.Match(currTxt_I, valusePattern).Groups[1].Value;
+                                var ctrlIcons = srcValue.Split('|');
+                                foreach (var item in ctrlIcons)
+                                {
+                                    if (urlIdDic.ContainsKey(item) == true)
+                                    {
+                                        idNameDic.Remove(urlIdDic[item]);
+                                    }
+                                }
+
+                                string defalutValue = Regex.Match(currTxt_I, defaultPattern).Groups[1].Value;
+                                if (urlIdDic.ContainsKey(defalutValue) == true)
+                                {
+                                    idNameDic.Remove(urlIdDic[defalutValue]);
+                                }
+                            }
+                        }
+                        //}
+                    }
+                }
+            }
+            List<MySortPng> listSort = new List<MySortPng>();
+            foreach (var item in idNameDic)
+            {
+                if (File.Exists(pathPngDic[item.Key]))
+                {
+                    Image image = Image.FromFile(pathPngDic[item.Key]);
+                    listSort.Add(new MySortPng
+                    {
+                        rectArea = (image.Width * image.Height),
+                        outLineTxt = string.Format("{0}    长{1},高{2},面积{3}", item.Value, image.Width, image.Height, (image.Width * image.Height))
+                    });
+                    image.Dispose();
+                }
+            }
+            var newlistSort = listSort.OrderByDescending(o => o.rectArea);
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("已为倒序了 优先处理大的碎图\r\n");
+            foreach (var item in newlistSort)
+            {
+                sb.AppendLine(item.outLineTxt);
+            }
+            this.txtConsole.Text = sb.ToString();
+
+
+            Console.WriteLine("共计 " + idNameDic.Count);
+        }
+
+
+        private void fguiPKGTxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            string txtContent;
+            if (File.Exists(mFGUIPath))
+            {
+                txtContent = File.ReadAllText(mFGUIPath);
+            }
+            else
+            {
+                txtContent = @"G:\Bingganren2021_SVN\client\FguiProject\assets_*_Common;Items;Font_*_Common_*_Items;Common";
+                File.WriteAllText(mFGUIPath, txtContent);
+            }
+            var strs = txtContent.Split("_*_");
+            this.fguiPath.Text = strs[0];
+            this.ignoreTxt.Text = strs[1];
+            this.fguiPKGTxt.Text = strs[2];
+            this.ignoreIconCommon.Text = strs[3];
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class MySortPng
+    {
+        public string outLineTxt { get; set; }
+        public int rectArea { get; set; }
+    }
+}
